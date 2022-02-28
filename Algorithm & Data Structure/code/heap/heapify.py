05d85545eed3820d1151da24a9d712ead8258c04
DaonWoori <88466357+DaonWoori@users.noreply.github.com>
2022-02-28 10:24:52 +0900

    Update heapify.py

--- Algorithm & Data Structure/code/heap/heapify.py
+++ Algorithm & Data Structure/code/heap/heapify.py
@@ -12,7 +12,7 @@ def swap(tree, index_1, index_2):
     tree[index_2] = temp
 
 def heapity(tree, index, tree_size):
-    """heapify 함수"""
+    """heapify 함수 - 정답 코드"""
     # 왼쪽 자식 노드의 인덱스와 오른쪽 자식 노드의 인덱스를 계산
     left_child_index = index * 2
     right_child_index = index * 2 + 1
@@ -31,6 +31,20 @@ def heapity(tree, index, tree_size):
     if largest != index: # 부모 노드의 값이 자식 노드의 값보다 작으면
         swap(tree, index, largest) # 부모 노드와 최댓값을 가진 자식 노드의 위치를 바꿔준다
         heapity(tree, largest, tree_size) # 자리가 바뀌어 자식 노드가 된 기존의 부모 노드를 대상으로 또 heapify 함수를 호출한다
+ 
+def heapify(tree, index, tree_size):
+    """heapify 함수 - 내가 구현한 방식"""
+
+    # 왼쪽 자식 노드의 인덱스와 오른쪽 자식 노드의 인덱스를 계산
+    left_child_index = 2 * index
+    right_child_index = 2 * index + 1
+
+    if 0 < left_child_index < tree_size and 0 < right_child_index < tree_size:
+        max_index = left_child_index if tree[left_child_index] > tree[right_child_index] else right_child_index
+
+        if tree[max_index] > tree[index]:
+            swap(tree, index, max_index)
+            heapify(tree, max_index, tree_size)
 
 # 실행 코드
 tree = [None, 15, 5, 12, 14, 9, 10, 6, 2, 11, 1] # heapify하려고 하는 완전 이진 트리
