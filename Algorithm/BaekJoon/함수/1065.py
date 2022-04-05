f192e8b85d13612b3b7329f47b3a8432bc7b893d
DaonWoori <88466357+DaonWoori@users.noreply.github.com>
2022-04-05 09:41:45 +0900

    Update 1065.py

--- Algorithm/BaekJoon/함수/1065.py
+++ Algorithm/BaekJoon/함수/1065.py
@@ -6,11 +6,8 @@ def hansu(num):
   else:
     cnt = 99
     for i in range(100, num + 1):
-      hund = (i//100)
-      ten = ((i%100)//10)
-      one = ((i%100)%10)
-
-      if hund - ten == ten - one:
+      nums = list(map(int, str(i)))
+      if nums[0] - nums[1] == nums[1] - nums[2]:
         cnt += 1
 
   return cnt
